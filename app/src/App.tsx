2efffaceb0e574c4cb62bae8d2f8ba4afe23e802
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2021-04-08 18:39:16 +0100

    graphql & ApolloClient set

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,19 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        Hello
+      </div>
+    </ApolloProvider>
   );
 }
 
