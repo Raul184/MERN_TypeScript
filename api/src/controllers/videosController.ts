2afeaca9fbac68ef314bb7f572122c39a379d0b4
olo <snash@tutanota.com>
2021-05-04 15:32:11 +0100

    route & controller for GetAllVideos from DB set

--- api/src/controllers/videosController.ts
+++ api/src/controllers/videosController.ts
@@ -2,8 +2,14 @@ import { RequestHandler } from 'express'
 import Video from '../models/Video'
 
 
-export const getVideos: RequestHandler = (req, res) => {
-  return res.json('Getting videos')
+export const getVideos: RequestHandler = async (req, res) => {
+  try {
+    const videos = await Video.find();
+    return res.json(videos)
+  }
+  catch (err) {
+    return res.json(err.message)
+  }
 }
 
 export const getVideo: RequestHandler = (req, res) => {
@@ -11,6 +17,11 @@ export const getVideo: RequestHandler = (req, res) => {
 }
 
 export const createVideo: RequestHandler = async (req, res) => {
+  const url = await Video.findOne({ url: req.body.url })
+  if (url) return res.status(301).json({
+    msg: "Sorry, this video is already available"
+  })
+
   const video = new Video(req.body)
   const vSaved = await video.save()
   return res.json(vSaved)
