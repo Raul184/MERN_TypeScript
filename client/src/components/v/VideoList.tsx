d2e3ff6f786b2b9bc684c0bf2b052baed43a4a9a
olo <snash@tutanota.com>
2021-05-06 12:34:45 +0100

    setting components & react router

---
+++ client/src/components/v/VideoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoList = () => {
+  return (
+    <div>
+      List
+    </div>
+  )
+}
+
+export default VideoList;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './index.css';
+import VideoList from './components/v/VideoList'
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <h1>Hi</h1>
+    <Router>
+      <Switch>
+        <Route path='/' component={VideoList} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
