8a456a93ec4c2fa9a38ff3b23ba550978506e9dd
olo <snash@tutanota.com>
2021-05-06 12:44:40 +0100

    react router set

---
+++ client/src/components/videoForm/VideoForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const VideoForm = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+export default VideoForm;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,16 @@ import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
-import VideoList from './components/v/VideoList'
+import VideoList from './components/videoList/VideoList'
+import VideoForm from './components/videoForm/VideoForm';
 
 render(
   <React.StrictMode>
     <h1>Hi</h1>
     <Router>
       <Switch>
-        <Route path='/' component={VideoList} />
+        <Route exact path='/' component={VideoList} />
+        <Route path='/new' component={VideoForm} />
       </Switch>
     </Router>
   </React.StrictMode>,
