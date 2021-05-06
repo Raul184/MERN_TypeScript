0ddf9ea373d43b220ebaf47916e63ce7f748c418
olo <snash@tutanota.com>
2021-05-06 13:14:34 +0100

    home pg in progress

--- client/src/components/videoList/VideoList.tsx
+++ client/src/components/videoList/VideoList.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const VideoList = () => {
+  useEffect(() => {
+    loadVideos()
+  }, [])
+  const loadVideos = async () => {
+    const videos = await fetch('http://localhost:4000/videos')
+    const json = await videos.json()
+    console.log('Videos', json);
+  }
   return (
     <div>
       List
