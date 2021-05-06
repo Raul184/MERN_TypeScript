1651bff01ead159fa6ba638c0050e805c55b9a20
olo <snash@tutanota.com>
2021-05-06 16:44:54 +0100

    form pg in progress

--- client/src/components/videoForm/VideoForm.tsx
+++ client/src/components/videoForm/VideoForm.tsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 const VideoForm = () => {
   return (
-    <div>
-
+    <div className="row">
+      <div className="col-md-4 offset-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h3>New Video</h3>
+            <form action="">
+              <div style={{margin: '20px 10px'}}>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Please input a title"
+                  className="form-control"
+                  autoFocus
+                />
+              </div>
+              <div style={{margin: '20px 10px'}}>
+                <input
+                  type="text"
+                  name="url"
+                  placeholder="...someWeb.com"
+                  className="form-control"
+                />
+              </div>
+              <div style={{margin: '20px 10px'}}>
+                <textarea
+                  name="Description"
+                  rows={3}
+                  className="form-control"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
