c69bcaddebfaad33d8a1944ea2c29d78637b04ff
olo <snash@tutanota.com>
2021-05-06 16:01:45 +0100

    nav set

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,17 @@ import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
+import 'bootswatch/dist/pulse/bootstrap.min.css'
+
 import VideoList from './components/videoList/VideoList'
 import VideoForm from './components/videoForm/VideoForm';
+import Nav from './components/nav/Nav';
 
 render(
   <React.StrictMode>
     <h1>Hi</h1>
     <Router>
+      <Nav />
       <Switch>
         <Route exact path='/' component={VideoList} />
         <Route path='/new' component={VideoForm} />
